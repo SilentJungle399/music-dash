8d925fef31525a73703c6496635253bb9e77c0fa
SilentJungle399 <silentjungle399@gmail.com>
2021-12-19 10:18:41 +0100

    ping cmd and some shet

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,4 +1,5 @@
 from client import Client
+import time
 
 bot = Client(token = "NzQwNTY4NzY2MTk4NDQ4MTkw.Xyq6aA.ryjwwbMeF8rUnjBJwZiLP3_uaAk")
 
@@ -7,5 +8,14 @@ async def test(ctx):
 	player = bot.lavalink.player_manager.get(ctx.guild.id)
 
 	await ctx.send(str(player.current))
+
+@bot.command()
+async def ping(ctx):
+	t1 = time.monotonic()
+	await ctx.trigger_typing()
+
+	t2 = time.monotonic()
+
+	await ctx.send(f"Pong! `{round((t2 - t1) * 1000)}ms`")
 
 bot.initiate()
